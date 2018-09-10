3cc8c501be5d757f2ab6419f966a63fd0fd4bc67
XorMasters <aknight@vcccd.edu>
2018-09-10 12:27:17 -0700

    Checking if assignment works at VC

---
+++ include/binarysearch.h
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+
+namespace edu { namespace vcccd { namespace vc { namespace csv15 {
+    template <class T>
+    int64_t binarysearch(const T &value, T array[], size_t size) {
+        size_t curr = size / 2;
+        for(size_t hi = size - 1, lo = 0; hi - lo > 0; curr = (hi + lo)/2) {
+            if (value > array[curr]) {
+                lo = curr + 1;
+            } else if (value < array[curr]) {
+                hi = curr - 1;
+            } else {
+                return curr;
+            }
+        }
+        return array[curr] == value ? curr : -1;
+    }
+}}}}
